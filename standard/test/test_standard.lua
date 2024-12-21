bfe7c9f3467ee373ddce711def4dde815da76b5e
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2024-12-22 03:04:15 +0800

    洛神

--- standard/test/test_standard.lua
+++ standard/test/test_standard.lua
@@ -143,6 +143,33 @@ function TestStandard:testLuoYi()
   lu.assertEquals(comp2.hp, origin_hp - 1)
 end
 
+function TestStandard:testLuoShen()
+  local room = LRoom ---@type Room
+  local me = room.players[1] ---@type ServerPlayer
+
+  RunInRoom(function()
+    room:handleAddLoseSkills(me, "luoshen")
+  end)
+  local red = table.find(room.draw_pile, function(cid)
+    return Fk:getCardById(cid).color == Card.Red
+  end)
+  local blacks = table.filter(room.draw_pile, function(cid)
+    return Fk:getCardById(cid).color == Card.Black
+  end)
+  local rnd = 5
+  SetNextReplies(me, { "1", "1", "1", "1", "1", "1" }) -- 除了第一个1以外后面全是潜在的“重复流程”
+  -- 每次往红牌顶上塞若干个黑牌
+  RunInRoom(function()
+    room:throwCard(me:getCardIds("h"), nil, me, me)
+    -- 控顶
+    room:moveCardTo(red, Card.DrawPile)
+    if rnd > 0 then room:moveCardTo(table.slice(blacks, 1, rnd + 1), Card.DrawPile) end
+
+    GameEvent.Turn:create(me, { phase_table = { Player.Start } }):exec()
+  end)
+  lu.assertEquals(#me:getCardIds("h"), rnd)
+end
+
 function TestStandard:testMashu()
   local room = LRoom ---@type Room
   local me = room.players[1] ---@type ServerPlayer
