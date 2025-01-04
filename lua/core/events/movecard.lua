629af84878e5b76b0823e228640db51c0920348d
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2025-01-05 05:11:50 +0800

    初填data

--- lua/core/events/movecard.lua
+++ lua/core/events/movecard.lua
@@ -1,3 +1,23 @@
+
+--- MoveCardsData 移动牌的数据
+---@class MoveCardsDataSpec
+---@field public moveInfo MoveInfo[] @ 移动信息
+---@field public from ServerPlayer? @ 移动来源玩家
+---@field public to ServerPlayer? @ 移动终点玩家
+---@field public toArea CardArea @ 移动终点区域
+---@field public moveReason CardMoveReason @ 移动原因
+---@field public proposer? ServerPlayer @ 移动执行者
+---@field public skillName? string @ 移动技能名
+---@field public moveVisible? boolean @ 控制移动是否可见
+---@field public specialName? string @ 若终点区域为PlayerSpecial，则存至对应私人牌堆内
+---@field public specialVisible? boolean @ 控制上述创建私人牌堆后是否令其可见
+---@field public drawPilePosition? integer @ 移至牌堆的索引位置，值为-1代表置入牌堆底，或者牌堆牌数+1也为牌堆底
+---@field public moveMark? table|string @ 移动后自动赋予标记，格式：{标记名(支持-inarea后缀，移出值代表区域后清除), 值}
+---@field public visiblePlayers? integer|integer[] @ 控制移动对特定角色可见（在moveVisible为false时生效）
+
+---@class MoveCardsData: MoveCardsDataSpec, TriggerData
+MoveCardsData = TriggerData:subclass("MoveCardsData")
+
 ---@class MoveCardsEvent: TriggerEvent
 ---@field data MoveCardsData
 local MoveCardsEvent = TriggerEvent:subclass("MoveCardsEvent")
@@ -9,3 +29,11 @@ fk.AfterCardsMove = MoveCardsEvent:subclass("fk.AfterCardsMove")
 ---@class fk.BeforeDrawCard: MoveCardsEvent
 fk.BeforeDrawCard = MoveCardsEvent:subclass("fk.BeforeDrawCard")
 
+-- 注释
+
+---@alias MoveCardsFunc fun(self: TriggerSkill, event: MoveCardsEvent,
+---  target: ServerPlayer, player: ServerPlayer, data: MoveCardsData): any
+
+---@class SkillSkeleton
+---@field public addEffect fun(self: SkillSkeleton, key: MoveCardsEvent,
+---  attr: TrigSkelAttribute?, data: TrigSkelSpec<MoveCardsFunc>): SkillSkeleton
